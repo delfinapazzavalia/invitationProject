5d40d13892f11eb90e545db662c170282da5c6c8
“delfinapazzavaliagit config --global user.email “delfinapazzavalia@gmail.com <delfinapazzavalia@gmail.com>
2023-11-27 12:52:40 -1000

    navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <>
-    <h1>NAVBAR</h1>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <nav className="navbar">
+                    <a href='#Home' className="navbar-left">NOMBRE & NOMBRE</a>
+                    <div className="navbar-right">
+                        <a href="#AboutMe">Confirmacion</a>
+                        <a href="#Resume">Ceremonia</a>
+                        <a href="#Projects">Fiesta</a>
+                        <a href="#Skills">Regalos</a>
+                        <a href="#Skills">Mas</a>
+
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
