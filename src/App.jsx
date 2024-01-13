75983cbf7b55cae12b3c8ba38708b3339b857a5e
martinasorialanzi <martinasorialanzi@gmail.com>
2024-01-13 20:27:47 -0300

    app routes fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Event from './components/Event';
-import Confirmation from './components/Confirmation';
-import Gift from './components/Gift';
-import Footer from './components/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
+import Event from './components/event/Event';
+import Confirmation from './components/confirmation/Confirmation';
+import Gift from './components/gift/Gift';
+import Footer from './components/footer/Footer';
 import Music from './components/music/Music';
 import Memories from './components/memories/Memories';
 
