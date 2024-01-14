02ca09f6735766480e36b5b0c94798e9213889ae
martinasorialanzi <martinasorialanzi@gmail.com>
2024-01-13 23:52:12 -0300

    layout music section

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Col, Row, Container, Carousel } from "react-bootstrap";
 import "../event/event.css";
 import img1 from "../../assets/images/eventSection/Carousel/img1.png";
+import { Link } from "react-router-dom";
 
 const Event = () => {
   return (
@@ -39,10 +40,14 @@ const Event = () => {
           <div>
             <p className="subtitleEventSection">Ceremonia</p>
             <p className="textEventSection">Iglesia Nuestra Señora del Valle<br/>17hs</p>
+            <Link target="_blank" to={"https://www.google.com/maps/place/Parroquia+Nuestra+Se%C3%B1ora+del+Valle/@-26.815749,-65.293438,16z/data=!4m6!3m5!1s0x942242d62123af13:0xaa2edb3420b1b358!8m2!3d-26.815749!4d-65.293438!16s%2Fg%2F1tfwlfys?hl=es&entry=ttu"}>
             <Button className="buttonEventSection">Como llegar</Button>
+            </Link>
             <p className="subtitleEventSection">Fiesta</p>
             <p className="textEventSection">San Pablo Country Club <br/>19hs</p>
+            <Link target="_blank" to={"https://www.google.com/maps/place/San+Pablo+Country+Life+%26+Golf/@-26.8539783,-65.3181571,17z/data=!3m1!4b1!4m6!3m5!1s0x9422436f81cdc72d:0x2f083f34b254a65e!8m2!3d-26.8539831!4d-65.3155822!16s%2Fg%2F11b7tv2yz7?hl=es&entry=ttu"}>
             <Button className="buttonEventSection">Como llegar</Button>
+            </Link>
           </div>
 
           <iframe
