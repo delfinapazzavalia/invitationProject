dd6143ab21d23a2b0ad37bcd1b6dffc3a8ca2cf8
Victoria <victoriamariateran@gmail.com>
2024-01-14 00:00:25 -1000

    maqueta confirmation y gift

--- src/components/confirmation/Confirmation.jsx
+++ src/components/confirmation/Confirmation.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './confirmation.css';
 
 const Confirmation = () => {
   return (
     <>
-    <h1>
-        CONFIRMATION
-    </h1>
+    <div className='general-confirmation'>
+    <h2>
+        Esperamos tu confirmacion
+    </h2>
+    <p className='parrafo-confirmation'>
+    Nuestro festejo no sería lo mismo sin vos. Confirmanos tu presencia a traves del enlace a continuación, y anota cualquier restricción dietética o petición especial.
+    </p>
+    <button className='button-confirmation'>
+      Confirmar presencia
+    </button>
+    </div>
     </>
   )
 }
