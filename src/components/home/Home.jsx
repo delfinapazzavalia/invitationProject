deff22e1f00bd157fafb64bb52cd98802873f4da
“delfinapazzavalia” <delfinapazzavalia@gmail.com>
2024-01-18 18:08:46 -1000

    countDown

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,18 +3,27 @@
 import React from 'react';
 import imgHome from '../home/imgNoDispo.webp';
 import '../home/home.css'
+import CountdownTimer from '../home/CountDown';
 
 const Home = () => {
+  const targetDate = new Date('2024-01-31T00:00:00');
   return (
-    <div className="image-with-headings-container">
-      <img src={imgHome} alt="Imagen" className="image" />
-      <div className="headings-container">
-        <h1>Nombre & Nombre</h1>
-        <h2>Save the date</h2>
-        <h3>11 de nov de 2024</h3>
-        <h3>Cuenta regresiva</h3>
-      </div>
+    <div id="Home" className="home">
+
+    <div className="home-left">
+      <h1 className="home-welcome"><strong> Nombre & Nombre </strong></h1>
+      <h2>Save the date</h2>
+      <span>11 de Noviembre de 2024</span>
+      <br />
+      <br />
+      <CountdownTimer targetDate={targetDate} />
+      <br />
+      <br />
     </div>
+    <div className="home-right">
+      <img src={imgHome} alt="Profile" />
+    </div>
+  </div>
   );
 };
 
